c599a0e8f2d6350c8738ac8342392923348aaea4
jinfeng <jinfeng.zhuang@aliyun.com>
2018-06-23 00:24:10 +0800

    core:delay: add limit check

--- packages/include/delay.h
+++ packages/include/delay.h
@@ -3,14 +3,21 @@
 
 #include <stdint.h>
 
+#define UDELAY_LIMIT	(0xFFFFFFFF / 1000)
+
 static inline void udelay(uint32_t us)
 {
-	for (volatile int i=0; i<us*CONFIG_CORE_CYCLE_PER_US; i++);
+	// TODO
+	if (CONFIG_CORE_CYCLE_PER_US > UDELAY_LIMIT) {
+		while(1);
+	}
+
+	for (volatile unsigned int i=0; i<us*CONFIG_CORE_CYCLE_PER_US; i++);
 }
 
 static inline void mdelay(uint32_t ms)
 {
-	for (volatile int i=0; i<ms; i++) {
+	for (volatile unsigned int i=0; i<ms; i++) {
 		udelay(1000);
 	}
 }
