e787d65c2ff1f9e74a36abd674483367082d46c1
jinfeng <jinfeng.zhuang@aliyun.com>
2018-06-23 22:39:38 +0800

    lld: nvic: add empty driver

---
+++ packages/lld_arm_nvic/nvic.c
@@ -0,0 +1,19 @@
+#include <io.h>
+#include <stdint.h>
+
+void interrupt_controller_init (void)
+{
+}
+
+void interrupt_controller_ack (void)
+{
+}
+
+void interrupt_controller_enable (int irq)
+{
+}
+
+void interrupt_controller_disable (int irq)
+{
+}
+
