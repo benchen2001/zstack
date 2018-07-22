0a0126e0dd01da57148904c3fa59ff6a3ffad5e2
jinfeng <jinfeng.zhuang@aliyun.com>
2018-07-22 12:14:54 +0800

    include: add TRACE for easy debug

--- packages/include/debug.h
+++ packages/include/debug.h
@@ -1,7 +1,7 @@
 #ifndef DEBUG_H
 #define DEBUG_H
 
-#define CALLSTACK \
-    printf("Callstack: %s %s %d\n", __FILE__, __FUNCTION__, __LINE__)
+#define CALLSTACK	printf("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__)
+#define TRACE		printf("%s %s %d\n", __FILE__, __FUNCTION__, __LINE__)
 
 #endif
